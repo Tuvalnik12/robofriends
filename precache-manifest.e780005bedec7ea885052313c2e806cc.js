0051a8f644d095d3ad0dc578737da07be56e466c
Tuvalnik12 <milecki46@gmail.com>
2019-10-17 15:29:12 +0300

    Updates

---
+++ precache-manifest.e780005bedec7ea885052313c2e806cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96e54d4ed1e46382dc9533d8eb71f13",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "bab79a596a336ca11327",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "553980642cb73e3421c2",
+    "url": "/robofriends/static/css/main.eb864b85.chunk.css"
+  },
+  {
+    "revision": "bab79a596a336ca11327",
+    "url": "/robofriends/static/js/2.0b817237.chunk.js"
+  },
+  {
+    "revision": "553980642cb73e3421c2",
+    "url": "/robofriends/static/js/main.f28a7ee7.chunk.js"
+  },
+  {
+    "revision": "3b225231780738bdf4d7",
+    "url": "/robofriends/static/js/runtime~main.c053eeb9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
